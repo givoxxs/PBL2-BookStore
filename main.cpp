29b0333876470c7c1c3115c3911c21b7eb3c8472
givoxxs <toanyogame@gmail.com>
2023-11-05 12:58:34 +0700

    fix Giao dien

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include "Book.h"
 #include "Staff.h"
 #include "MyLib.h"
+#include "MyMenu.h"
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include <windows.h>
 
 std::vector<Staff> Staff_Data;
 std::vector<Book> BookInventory;
@@ -16,129 +16,121 @@ void Readfile();
 void Add_Book();
 void QuanlyNhanvien();
 void QuanlySach();
+void Main_Menu();
 
 int main() {
     Intro_start();
     lay_du_lieu();
     Readfile();
 
-    int choice;
-
-    do {
-        std::cout << "\t\t+------------ QUAN LY CUA HANG ------------+\n";
-        std::cout << "\t\t|  1. Kho SACH                             |\n";
-        std::cout << "\t\t+------------------------------------------+\n";
-        std::cout << "\t\t|  2. Danh sach Nhan vien                  |\n";
-        std::cout << "\t\t+------------------------------------------+\n";
-        std::cout << "\t\t|  3. Danh sach Khach hang                 |\n";
-        std::cout << "\t\t+------------------------------------------+\n";
-        std::cout << "\t\t|  4. Lich su giao dich                    |\n";
-        std::cout << "\t\t+------------------------------------------+\n";
-        std::cout << "\t\t|  0. Thoat!                               |\n";
-        std::cout << "\t\t+------------------------------------------+\n";
-        std::cout << "\t\t|   Nhap Lua Chon:      ";
-        std::cin >> choice;
-
-        switch (choice) {
-            case 1:
-                std::cout << "\n\t\t*QUAN LY SACH TROG CUA HANG*\n";
-                QuanlySach();
-                break;
-            case 2:
-                std::cout << "\n\t\t*QUAN LY NHAN VIEN TROG CUA HANG*\n";
-                QuanlyNhanvien();
-                break;
-            case 3:
-                std::cout << "\n\t\t*QUAN LY KHACH HANG TROG CUA HANG*\n";
-                // QuanlyKhachhang();
-                break;
-            case 4:
-                std::cout << "\n\t\t*Giao dich cua CUA HANG SACH*\n";
-                break;
-            case 0:
-                return 0; // Thoát khỏi chương trình
-            default:
-                std::cout << "Nhap sai vui long lua chon lai.\n";
-        }
-    } while (1);
+    Main_Menu();
 
     return 0;
 }
 
-void QuanlySach() {
-    int choice;
-    while (1) {
-        std::cout << std::endl;
-        std::cout << "\t\t\t================== SACH ===================\n";
-        std::cout << "\t\t\t  1. Hien thi tat ca sach.                 \n";
-        std::cout << "\t\t\t  2. Them sach.                            \n";
-        std::cout << "\t\t\t  3. Tim kiem sach.                        \n";
-        std::cout << "\t\t\t  4. Xoa 1 quyen sach.                     \n";
-        std::cout << "\t\t\t  0. Thoat!                                \n";
-        std::cout << "\t\t\t===========================================\n";
-        std::cout << "\t\t\t   Nhap Lua Chon: ";
-        std::cin >> choice;
+void Main_Menu() {
+    int choose, n = 4;
+    string nd[n];
+    nd[0] = "Kho sach";
+    nd[1] = "Danh sach Nhan Vien";
+    nd[2] = "Danh sach Khach Hang";
+    nd[3] = "Thoat";
 
-        switch (choice) {
-            case 1:
+    do
+    {
+        system("cls");
+        choose = Menu(80, 5, 35, 2, 11, 75, nd, n);
+        system("cls");
+        switch (choose)
+        {
+        case 0:
+            std::cout << "\n\t\t*QUAN LY SACH TROG CUA HANG*\n";
+            QuanlySach();
+            break;
+        case 1:
+            std::cout << "\n\t\t*QUAN LY NHAN VIEN TROG CUA HANG*\n";
+            QuanlyNhanvien();
+            break;
+        case 2:
+            std::cout << "\n\t\t*QUAN LY KHACH HANG TROG CUA HANG*\n";
+            // QuanlyKhachhang();
+            break;
+        case 3:
+            //system("pause");
+            break;
+        }
+    } while (n - choose - 1);
+}
+
+
+void QuanlySach() {
+
+    int choose, n = 4;
+    string nd[n];
+    nd[0] = "Hien thi tat ca sach";
+    nd[1] = "Them sach";
+    nd[2] = "Tim kiem sach";
+    nd[3] = "Thoat!";
+
+    do {
+            system("cls");
+            choose = Menu(80, 5, 35, 2, 11, 75, nd, n);
+            system("cls");
+            switch (choose)
+            {
+            case 0:
                 std::cout << "\n\t\t*THONG TIN SACH TRONG CUA HANG*\n";
                 Book::display_All(BookInventory);
+                system("pause");
                 break;
-            case 2:
+            case 1:
                 std::cout << "\n\t\t*THEM 1 SACH VAO KHO SACH*\n";
                 Add_Book();
+                system("pause");
                 break;
-            case 3:
+            case 2:
                 std::cout << "\n\t\t*TIM KIEM SACH TRONG KHO SACH*\n";
                 Book::search(BookInventory);
+                system("pause");
                 break;
-            case 4:
-                std::cout << "\n\t\t*XOA 1 quyen sach*\n";
+            case 3:
+                //system("pause");
                 break;
-            case 0:
-                return; // Thoát khỏi chương trình
-            default:
-                std::cout << "Nhap sai vui long lua chon lai.\n";
-        }
-    }
+            }
+        } while (n - choose - 1);
 }
 
 void QuanlyNhanvien() {
-    int choice;
-    while (1) {
-        std::cout << std::endl;
-        std::cout << "\t\t\t===============  NHAN VIEN ================\n";
-        std::cout << "\t\t\t  1. Hien thi tat ca nhan vien.            \n";
-        std::cout << "\t\t\t  2. Them moi 1 nhan vien.                 \n";
-        std::cout << "\t\t\t  3. Tim kiem.                             \n";
-        std::cout << "\t\t\t  4. hello world.                          \n";
-        std::cout << "\t\t\t  0. Thoat!                                \n";
-        std::cout << "\t\t\t===========================================\n";
-        std::cout << "\t\t\t   Nhap Lua Chon: ";
-        std::cin >> choice;
+    int choose, n = 4;
+    string nd[n];
+    nd[0] = "Hien thi tat ca nhan vien";
+    nd[1] = "Them moi 1 nhan vien";
+    nd[2] = "Tim kiem";
+    nd[3] = "Thoat!";
 
-        switch (choice) {
-            case 1:
+    do {
+            system("cls");
+            choose = Menu(80, 5, 35, 2, 11, 75, nd, n);
+            system("cls");
+            switch (choose)
+            {
+            case 0:
                 std::cout << "\n\t\t*DANH SACH NHAN VIEN TRONG CUA HANG*\n";
                 Staff::displayAll(Staff_Data);
                 break;
-            case 2:
+            case 1:
                 std::cout << "\n\t\t*THEM 1 NHAN VIEN*\n";
                 Staff::Add(Staff_Data);
                 break;
-            case 3:
+            case 2:
                 std::cout << "\n\t\t*TIM KIEM*\n";
                 Staff::search(Staff_Data);
                 break;
-            case 4:
-                std::cout << "\n\t\t*HELLO WORLD*\n";
+            case 3:
+                //system("pause");
                 break;
-            case 0:
-                return; // Thoát khỏi chương trình
-            default:
-                std::cout << "\n\t\tNhap sai vui long lua chon lai.\n";
-        }
-    }
+            }
+        } while (n - choose - 1);
 }
 
 void setspace(int n) {
@@ -150,7 +142,7 @@ void Intro_start() {
     int n = 20;
     setspace(n - 5);
     for (int i = 0; i < 60; i++)
-        std::cout << "=";
+        std::cout << char(196);;
     std::cout << std::endl;
     setspace(5 + n);
     std::cout << "Truong Dai hoc Bach Khoa - Dai hoc Da Nang" << std::endl;
@@ -174,9 +166,9 @@ void Intro_start() {
     std::cout << "-Nguyen Le Nhat Tuan\n";
     setspace(n - 5);
     for (int i = 0; i < 60; i++)
-        std::cout << "=";
+        std::cout << char(196);;
     getchar();
-    std::cout << "\n\n";
+    system("cls");
 }
 
 void lay_du_lieu() {
